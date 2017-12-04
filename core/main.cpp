36d3599069ecccb804da39611214d182c318f79e
Jacob Lewallen <jlewallen@gmail.com>
2017-12-03 19:28:23 -0800

    Pull RadioHead into firmware-common core test to look at memory.

--- core/main.cpp
+++ core/main.cpp
@@ -4,6 +4,8 @@
 
 #include <Arduino.h>
 
+#include <RH_RF95.h>
+
 #include <sd_raw.h>
 #include <fkfs.h>
 #include <fkfs_log.h>
@@ -20,6 +22,10 @@ extern "C" {
 
 const uint8_t WIFI_PIN_CS = 8;
 
+const uint8_t RFM95_PIN_CS = 8;
+const uint8_t RFM95_PIN_RST = 4;
+const uint8_t RFM95_PIN_INT = 3;
+
 const uint8_t SD_PIN_CS = 10;
 const uint8_t FKFS_FILE_LOG = 0;
 const uint8_t FKFS_FILE_PRIORITY_LOWEST = 255;
@@ -27,6 +33,8 @@ const uint8_t FKFS_FILE_PRIORITY_HIGHEST = 0;
 
 static fkfs_t fs = { 0 };
 static fkfs_log_t fkfs_log = { 0 };
+
+RH_RF95 rf95(RFM95_PIN_CS, RFM95_PIN_INT); 
 
 void debug_write_log(const char *str, void *arg) {
     fkfs_log_append(&fkfs_log, str);
@@ -40,11 +48,18 @@ size_t fkfs_log_message(const char *f, ...) {
     return 0;
 }
 
+bool setupRadio() {
+    if (!rf95.init()) {
+        debugfpln("Core", "LoRa missing (%d)", sizeof(rf95));
+    }
+
+    return false;
+}
+
 bool setupLogging() {
     fkfs_configure_logging(fkfs_log_message);
 
     if (!fkfs_create(&fs)) {
-        debugfln("fkfs_create failed");
         return false;
     }
 
@@ -52,30 +67,25 @@ bool setupLogging() {
     digitalWrite(SD_PIN_CS, HIGH);
 
     if (!sd_raw_initialize(&fs.sd, SD_PIN_CS)) {
-        debugfln("sd_raw_initialize failed");
         return false;
     }
 
     if (!fkfs_initialize_file(&fs, FKFS_FILE_LOG, FKFS_FILE_PRIORITY_LOWEST, false, "DEBUG.LOG")) {
-        debugfln("fkfs_initialize failed");
         return false;
     }
 
     if (!fkfs_log_initialize(&fkfs_log, &fs, FKFS_FILE_LOG)) {
-        debugfln("fkfs_log_initialize failed");
         return false;
     }
 
     if (false) {
         if (!fkfs_initialize(&fs, true)) {
-            debugfln("fkfs_initialize failed");
             return false;
         }
         fkfs_log_statistics(&fs);
     }
 
     if (!fkfs_initialize(&fs, false)) {
-        debugfln("fkfs_initialize failed");
         return false;
     }
     fkfs_log_statistics(&fs);
@@ -106,6 +116,10 @@ void setup() {
         while (true) {
             delay(10);
         }
+    }
+
+    if (!setupRadio()) {
+
     }
 
     fk::i2c_begin();
