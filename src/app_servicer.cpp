73fe1ed6c0e352e20d008db686ef7567af77e13a
Jacob Lewallen <jlewallen@gmail.com>
2017-12-04 11:45:57 -0800

    Stub out some of the other app queries.

--- src/app_servicer.cpp
+++ src/app_servicer.cpp
@@ -59,17 +59,80 @@ void AppServicer::handle(AppQueryMessage &query) {
 
         break;
     }
-    case fk_app_QueryType_QUEYR_CONFIGURE_SENSOR: {
-    }
+    case fk_app_QueryType_QUERY_DATA_SET:
     case fk_app_QueryType_QUERY_DATA_SETS: {
-    }
-    case fk_app_QueryType_QUERY_DATA_SET: {
+        log("Query ds");
+
+        fk_app_DataSet dataSets[] = {
+            {
+                .id = 0,
+                .sensor = 0,
+                .time = millis(),
+                .size = 100,
+                .pages = 10,
+                .hash = 0,
+                .name = {
+                    .funcs = {
+                        .encode = pb_encode_string,
+                    },
+                    .arg = (void *)"DS #1",
+                },
+            },
+        };
+
+        pb_array_t data_sets_array = {
+            .length = sizeof(dataSets) / sizeof(fk_app_DataSet),
+            .itemSize = sizeof(fk_app_DataSet),
+            .buffer = &dataSets,
+            .fields = fk_app_DataSet_fields,
+        };
+
+        AppReplyMessage reply(pool);
+        reply.m().type = fk_app_ReplyType_REPLY_DATA_SETS;
+        reply.m().dataSets.dataSets.funcs.encode = pb_encode_array;
+        reply.m().dataSets.dataSets.arg = (void *)&data_sets_array;
+        if (!outgoing.write(reply)) {
+            log("Error writing reply");
+        }
+
+        break;
     }
     case fk_app_QueryType_QUERY_DOWNLOAD_DATA_SET: {
+        log("Download ds %d page=%d", query.m().downloadDataSet.id, query.m().downloadDataSet.page);
+
+        uint8_t buffer[1024] = { 0 };
+        pb_data_t data = {
+            .length = 1024,
+            .buffer = buffer,
+        };
+
+        AppReplyMessage reply(pool);
+        reply.m().type = fk_app_ReplyType_REPLY_DOWNLOAD_DATA_SET;
+        reply.m().dataSetData.time = millis();
+        reply.m().dataSetData.page = query.m().downloadDataSet.page;
+        reply.m().dataSetData.data.funcs.encode = pb_encode_data;
+        reply.m().dataSetData.data.arg = (void *)&data;
+        reply.m().dataSetData.hash = 0;
+        if (!outgoing.write(reply)) {
+            log("Error writing reply");
+        }
+
+        break;
     }
     case fk_app_QueryType_QUERY_ERASE_DATA_SET: {
+        log("Erase ds");
+
+        AppReplyMessage reply(pool);
+        reply.m().type = fk_app_ReplyType_REPLY_SUCCESS;
+        if (!outgoing.write(reply)) {
+            log("Error writing reply");
+        }
+
+        break;
     }
     case fk_app_QueryType_QUERY_LIVE_DATA_POLL: {
+    }
+    case fk_app_QueryType_QUEYR_CONFIGURE_SENSOR: {
     }
     default: {
         AppReplyMessage reply(pool);
