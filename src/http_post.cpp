33e1a77a81afe87f9819a280f48cff77ee5a4b8e
Jacob Lewallen <jlewallen@gmail.com>
2017-12-06 21:58:10 -0800

    Encapsulate simple URL parsing.

--- src/http_post.cpp
+++ src/http_post.cpp
@@ -17,6 +17,26 @@ void HttpPost::done() {
     connected = false;
 }
 
+struct Url {
+public:
+    char *server{ nullptr };
+    char *path{ nullptr };
+
+public:
+    Url(char *url) {
+        for (auto p = url; p[0] != 0 && p[1] != 0; ++p) {
+            if (server == nullptr && p[0] == '/' && p[1] == '/') {
+                p += 2;
+                server = p;
+            } else if (server != nullptr && p[0] == '/') {
+                p[0] = 0;
+                path = p + 1;
+                break;
+            }
+        }
+    }
+};
+
 TaskEval HttpPost::task() {
     if (WiFi.status() != WL_AP_CONNECTED && WiFi.status() != WL_CONNECTED) {
         log("No Wifi, failing");
@@ -31,46 +51,39 @@ TaskEval HttpPost::task() {
     }
 
     if (!connected) {
-        const size_t length = strlen(config->url) + 1;
+        const auto length = strlen(config->url) + 1;
         char urlCopy[length];
-        char *server = nullptr;
-        char *path = nullptr;
         strncpy(urlCopy, config->url, length);
-        for (char *p = urlCopy; *p != 0; ++p) {
-            if (server == nullptr && p[0] == '/' && p[1] == '/') {
-                p += 2;
-                server = p;
-            } else if (server != nullptr && p[0] == '/') {
-                p[0] = 0;
-                path = p + 1;
-                break;
-            }
-        }
+        Url parsed(urlCopy);
 
         // TODO: Verify we got good values? Though this should
         // probably have been checked before.
-        log("Connecting: '%s' / '%s'", server, path);
+        if (parsed.server != nullptr && parsed.path != nullptr) {
+            log("Connecting: '%s' / '%s'", parsed.server, parsed.path);
 
-        if ((uint32_t)config->cachedAddress == (uint32_t)0) {
-            if (!WiFi.hostByName(server, config->cachedAddress)) {
-                log("DNS failure on '%s'", server);
+            if ((uint32_t)config->cachedAddress == (uint32_t)0) {
+                if (!WiFi.hostByName(parsed.server, config->cachedAddress)) {
+                    log("DNS failure on '%s'", parsed.server);
+                }
             }
-        }
 
-        // TODO: Fix blocking.
-        if (config->cachedAddress != (uint32_t)0 && wcl.connect(config->cachedAddress, 80)) {
-            connected = true;
-            log("Connected!");
-            wcl.print("GET /");
-            wcl.print(path);
-            wcl.println(" HTTP/1.1");
-            wcl.print("Host: ");
-            wcl.println(server);
-            wcl.println("Connection: close");
-            wcl.println();
+            // TODO: Fix blocking.
+            if (config->cachedAddress != (uint32_t)0 && wcl.connect(config->cachedAddress, 80)) {
+                connected = true;
+                log("Connected!");
+                wcl.print("GET /");
+                wcl.print(parsed.path);
+                wcl.println(" HTTP/1.1");
+                wcl.print("Host: ");
+                wcl.println(parsed.server);
+                wcl.println("Connection: close");
+                wcl.println();
+            } else {
+                log("Not connected!");
+                return TaskEval::yield(retry);
+            }
         } else {
-            log("Not connected!");
-            return TaskEval::yield(retry);
+            return TaskEval::error();
         }
     }
 
