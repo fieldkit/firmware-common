5763f5f9c6705819658b20a05333e0ce0659fe14
Jacob Lewallen <jlewallen@gmail.com>
2017-12-21 20:43:50 -0800

    LED control for our standard Fk 3 led array.

---
+++ src/leds.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+
+#include "leds.h"
+
+namespace fk {
+
+Leds::Leds() {
+}
+
+void Leds::idle() {
+    switch (status) {
+    case LedStatus::Fatal:  {
+        if (nextChange < millis()) {
+            // Simple toggle.
+            all(!digitalRead(A1));
+            nextChange = millis() + 100;
+        }
+        break;
+    }
+    default: {
+        break;
+    }
+    }
+}
+
+void Leds::setup() {
+    // Note that at least one board right now uses 13 for other purposes so
+    // ths should be done before that happens.
+    pinMode(13, OUTPUT);
+    digitalWrite(13, LOW);
+
+    pinMode(A3, OUTPUT);
+    pinMode(A4, OUTPUT);
+    pinMode(A5, OUTPUT);
+    off();
+}
+
+void Leds::all(bool value) {
+    digitalWrite(A3, value);
+    digitalWrite(A4, value);
+    digitalWrite(A5, value);
+}
+
+void Leds::on() {
+    all(HIGH);
+}
+
+void Leds::off() {
+    all(LOW);
+}
+
+void Leds::clear() {
+    status = LedStatus::None;
+    nextChange = 0;
+}
+
+void Leds::fatal() {
+    status = LedStatus::Fatal;
+    nextChange = 0;
+}
+
+}
---
+++ src/leds.h
@@ -0,0 +1,38 @@
+#ifndef LEDS_H_INCLUDED
+#define  LEDS_H_INCLUDED
+
+#include "active_object.h"
+
+namespace fk {
+
+enum class LedStatus {
+    None,
+    Fatal
+};
+
+class Leds : public ActiveObject {
+private:
+    LedStatus status{ LedStatus::None };
+    uint32_t nextChange{ 0 };
+
+public:
+    Leds();
+
+public:
+    void idle() override;
+
+public:
+    void setup();
+    void all(bool value);
+    void on();
+    void off();
+
+public:
+    void clear();
+    void fatal();
+
+};
+
+}
+
+#endif
