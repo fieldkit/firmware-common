83c76792daa9cb82822c62784f485367e68af0cb
Jacob Lewallen <jlewallen@gmail.com>
2018-06-27 13:04:58 -0700

    Nuke old data set handling code.

--- src/fkfs_replies.cpp
+++ src/fkfs_replies.cpp
@@ -50,64 +50,14 @@ Task *FkfsReplies::downloadFileReply(CoreState &state, AppQueryMessage &query, A
 }
 
 void FkfsReplies::eraseFileReply(AppQueryMessage &query, AppReplyMessage &reply, MessageBuffer &buffer) {
-    // fkfs_file_truncate(fs, query.m().eraseFile.id);
-
     queryFilesReply(query, reply, buffer);
 }
 
 void FkfsReplies::resetAll() {
-    /*
-    if (!fkfs_file_truncate_all(fs)) {
-        log("Error: Unable to truncateAll");
-    }
-
-    if (!fkfs_initialize(fs, true)) {
-        log("Error: Unable to resetAll");
-    }
-    */
-
     NVIC_SystemReset();
 }
 
 void FkfsReplies::dataSetsReply(AppQueryMessage &query, AppReplyMessage &reply, MessageBuffer &buffer) {
-    /*
-    fkfs_file_info_t fkfsFile;
-
-    // My plan is to pusht his into FkfsData and possibly consolidate fkfs handling.
-    fkfs_get_file(fs, dataFileId, &fkfsFile);
-
-    fk_app_DataSet dataSets[] = {
-        {
-            .id = dataFileId,
-            .sensor = 0,
-            .time = 0,
-            .size = fkfsFile.size,
-            .pages = fkfsFile.size / DefaultPageSize,
-            .hash = 0,
-            .name = {
-                .funcs = {
-                    .encode = pb_encode_string,
-                },
-                .arg = (void *)fkfsFile.name,
-            },
-        },
-    };
-
-    pb_array_t data_sets_array = {
-        .length = (size_t)(fkfsFile.size > 0 ? 1 : 0),
-        .itemSize = sizeof(fk_app_DataSet),
-        .buffer = &dataSets,
-        .fields = fk_app_DataSet_fields,
-    };
-
-    reply.m().type = fk_app_ReplyType_REPLY_DATA_SETS;
-    reply.m().dataSets.dataSets.funcs.encode = pb_encode_array;
-    reply.m().dataSets.dataSets.arg = (void *)&data_sets_array;
-
-    if (!buffer.write(reply)) {
-        log("Error writing reply");
-    }
-    */
 }
 
 Task *FkfsReplies::downloadDataSetReply(CoreState &state, AppQueryMessage &query, AppReplyMessage &reply, MessageBuffer &buffer, WifiConnection &connection) {
