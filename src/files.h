6dcd3490c1c8bba5409fe1f72b92249582eeae40
Jacob Lewallen <jlewallen@gmail.com>
2018-07-03 11:30:30 -0700

    Use new file descriptor ctors.

--- src/files.h
+++ src/files.h
@@ -65,11 +65,11 @@ private:
 
 class Files {
 private:
-    phylum::FileDescriptor file_system_area_fd =   { "system",        phylum::WriteStrategy::Rolling, 100 };
-    phylum::FileDescriptor file_log_startup_fd =   { "startup.log",   phylum::WriteStrategy::Append,  100 };
-    phylum::FileDescriptor file_log_now_fd =       { "now.log",       phylum::WriteStrategy::Rolling, 100 };
-    phylum::FileDescriptor file_log_emergency_fd = { "emergency.log", phylum::WriteStrategy::Append,  100 };
-    phylum::FileDescriptor file_data_fk =          { "data.fk",       phylum::WriteStrategy::Append,  0   };
+    phylum::FileDescriptor file_system_area_fd =   { "system",        100 };
+    phylum::FileDescriptor file_log_startup_fd =   { "startup.log",   100 };
+    phylum::FileDescriptor file_log_now_fd =       { "now.log",       100 };
+    phylum::FileDescriptor file_log_emergency_fd = { "emergency.log", 100 };
+    phylum::FileDescriptor file_data_fk =          { "data.fk",       0   };
     phylum::FileDescriptor* descriptors_[FileSystemNumberOfFiles]{
         &file_system_area_fd,
         &file_log_startup_fd,
