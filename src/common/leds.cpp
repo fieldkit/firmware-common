72b2b4806a376f3a66c977a66eaf0172a65c2d69
Jacob Lewallen <jlewallen@gmail.com>
2018-08-30 11:50:10 -0700

    New LED animations for the NeoPixel.

--- src/common/leds.cpp
+++ src/common/leds.cpp
@@ -6,6 +6,123 @@
 
 namespace fk {
 
+enum class AnimationType {
+    Off,
+    Blink,
+    Fade,
+    Wheel
+};
+
+static inline constexpr uint32_t get_color(uint8_t r, uint8_t g, uint8_t b) {
+    return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
+}
+
+template<typename T>
+struct Color {
+    T r{ 0 };
+    T g{ 0 };
+    T b{ 0 };
+
+    Color() {
+    }
+
+    Color(T r, T g, T b) : r(r), g(g), b(b) {
+    }
+
+    Color(uint32_t c) {
+        r = (c >> 16) & 0xff;
+        g = (c >> 8) & 0xff;
+        b = (c) & 0xff;
+    }
+
+    explicit operator uint32_t() {
+        return get_color(r, g, b);
+    }
+
+    Color operator * (uint32_t scalar) {
+        return { r * scalar, g * scalar, b * scalar };
+    }
+
+    Color operator / (uint32_t scalar) {
+        return { r / scalar, g / scalar, b / scalar };
+    }
+};
+
+class LedAnimation {
+private:
+    AnimationType type_{ AnimationType::Off };
+    uint32_t color_{ 0 };
+    uint16_t period_{ 0 };
+    uint16_t duration_{ 0 };
+    uint32_t started_{ 0 };
+    uint32_t active_{ 0 };
+
+public:
+    LedAnimation() {
+    }
+
+    LedAnimation(AnimationType type, uint32_t color, uint16_t period, uint16_t duration) :
+        type_(type), color_(color), period_(period), duration_(duration) {
+        started_ = fk_uptime();
+    }
+
+public:
+    bool off() {
+        return type_ == AnimationType::Off;
+    }
+
+public:
+    uint32_t update() {
+        auto elapsed = (fk_uptime() - started_);
+
+        if (duration_ > 0 && elapsed > duration_) {
+            type_ = AnimationType::Off;
+            return 0;
+        }
+
+        switch (type_) {
+        case AnimationType::Wheel: {
+            auto factor = (elapsed % period_) * 255 / period_;
+            auto position = 255 - (factor % 255);
+            if (position < 85) {
+                return get_color(255 - position * 3, 0, position * 3);
+            }
+            else if (position < 170) {
+                position -= 85;
+                return get_color(0, position * 3, 255 - position * 3);
+            }
+            else {
+                position -= 170;
+                return get_color(position * 3, 255 - position * 3, 0);
+            }
+            break;
+        }
+        case AnimationType::Blink: {
+            if ((elapsed % period_) < period_ / 2) {
+                return color_;
+            }
+            break;
+        }
+        case AnimationType::Fade: {
+            auto factor = elapsed % period_;
+            auto half = period_ / 2;
+            if (factor > (uint32_t)half) {
+                factor = period_ - factor;
+            }
+            auto c = Color<uint32_t>(color_) * factor / half;
+            return (uint32_t)c;
+        }
+        case AnimationType::Off:
+        default:
+            break;
+        }
+
+        return 0;
+    }
+};
+
+static LedAnimation active_;
+
 Leds::Leds() {
 }
 
@@ -29,6 +146,12 @@ void Leds::setup() {
 }
 
 void Leds::task() {
+    if (!active_.off()) {
+        auto color = active_.update();
+        pixel_.setBrightness(32);
+        pixel_.setPixelColor(0, color);
+        pixel_.show();
+    }
 }
 
 bool Leds::disabled() {
@@ -39,6 +162,7 @@ bool Leds::disabled() {
 }
 
 void Leds::notifyAlive() {
+    active_ = LedAnimation{ AnimationType::Fade, get_color(0, 0, 255), 500, 500 };
 }
 
 void Leds::notifyBattery(float percentage) {
@@ -60,13 +184,11 @@ void Leds::notifyReadingsDone() {
 }
 
 void Leds::notifyFatal() {
-    pixel_.setPixelColor(0, 64, 0, 0);
-    pixel_.show();
+    active_ = LedAnimation{ AnimationType::Blink, get_color(255, 0, 0), 500, 0 };
 }
 
 void Leds::notifyHappy() {
-    pixel_.setPixelColor(0, 16, 16, 16);
-    pixel_.show();
+    active_ = LedAnimation{ AnimationType::Wheel, 0, 5000, 0 };
 }
 
 }
